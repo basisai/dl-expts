1e20b1d0be68dca24792b45b35d0381b51780d2d
kesamet <kesamet@gmail.com>
2020-03-18 18:29:05 +0800

    Refactor

--- ocr/app_eng.py
+++ ocr/app_eng.py
@@ -24,13 +24,18 @@ FEATURE_LEN = 150
 ctx = mx.gpu(0) if mx.context.num_gpus() > 0 else mx.cpu()
 
 
-@st.cache()
-def segment(paragraph_segmented_image):
-    # Word segmentation
+@st.cache(allow_output_mutation=True)
+def load_segmentation_net(ctx):
     word_segmentation_net = WordSegmentationNet(2, ctx=ctx)
     word_segmentation_net.load_parameters(WORD_SEGMENTATION_MODEL)
     word_segmentation_net.hybridize()
+    return word_segmentation_net
 
+
+@st.cache
+def segment(paragraph_segmented_image, ctx):
+    # Word segmentation
+    word_segmentation_net = load_segmentation_net(ctx)
     min_c = 0.1
     overlap_thres = 0.1
     topk = 600
@@ -43,6 +48,15 @@ def segment(paragraph_segmented_image):
     return line_images
 
 
+@st.cache(allow_output_mutation=True)
+def load_recognition_net(ctx):
+    handwriting_line_recognition_net = HandwritingRecognitionNet(
+        rnn_hidden_states=512, rnn_layers=2, ctx=ctx, max_seq_len=160)
+    handwriting_line_recognition_net.load_parameters(HANDWRITING_RECOGNITION_MODEL, ctx=ctx)
+    handwriting_line_recognition_net.hybridize()
+    return handwriting_line_recognition_net
+
+
 def get_arg_max(prob):
     """Converts the output of the handwriting recognition network into strings."""
     arg_max = prob.topk(axis=2).asnumpy()
@@ -50,42 +64,22 @@ def get_arg_max(prob):
 
 
 @st.cache(suppress_st_warning=True)
-def recognize(line_images):
-    handwriting_line_recognition_net = HandwritingRecognitionNet(
-        rnn_hidden_states=512, rnn_layers=2, ctx=ctx, max_seq_len=160)
-    handwriting_line_recognition_net.load_parameters(HANDWRITING_RECOGNITION_MODEL, ctx=ctx)
-    handwriting_line_recognition_net.hybridize()
+def recognize(line_images, ctx):
+    handwriting_line_recognition_net = load_recognition_net(ctx)
 
     line_image_size = (60, 800)
-    form_character_prob = []
-    for i, line_image in enumerate(line_images):
-        line_image = handwriting_recognition_transform(line_image, line_image_size)
-        line_character_prob = handwriting_line_recognition_net(line_image.as_in_context(ctx))
-        form_character_prob.append(line_character_prob)
-
     progress_bar = st.progress(0)
     decoded_lines_am = []
-    for j, line_character_probs in enumerate(form_character_prob):
+    for j, line_image in enumerate(line_images):
+        line_image = handwriting_recognition_transform(line_image, line_image_size)
+        line_character_probs = handwriting_line_recognition_net(line_image.as_in_context(ctx))
         decoded_lines_am.append(get_arg_max(line_character_probs))
-        progress_bar.progress(int((j + 1) / len(form_character_prob) * 100))
+        progress_bar.progress(int((j + 1) / len(line_images) * 100))
     return decoded_lines_am
 
 
-def get_denoised(text, denoiser, generator):
-    """Denoise output."""
-    src_seq, src_valid_length = encode_char(text)
-    src_seq = mx.nd.array([src_seq], ctx=ctx)
-    src_valid_length = mx.nd.array(src_valid_length, ctx=ctx)
-    encoder_outputs, _ = denoiser.encode(src_seq, valid_length=src_valid_length)
-    states = denoiser.decoder.init_state_from_encoder(encoder_outputs,
-                                                      encoder_valid_length=src_valid_length)
-    inputs = mx.nd.full(shape=(1,), ctx=src_seq.context, dtype=np.float32, val=BOS)
-    output = generator.generate_sequences(inputs, states, text)
-    return output.strip()
-
-
-@st.cache(suppress_st_warning=True)
-def denoise(decoded_lines_am):
+@st.cache(allow_output_mutation=True)
+def load_denoiser(ctx):
     denoiser = Denoiser(alphabet_size=len(ALPHABET),
                         max_src_length=FEATURE_LEN,
                         max_tgt_length=FEATURE_LEN,
@@ -111,14 +105,32 @@ def denoise(decoded_lines_am):
                                   ctx,
                                   nlp.data.SacreMosesTokenizer(),
                                   nlp.data.SacreMosesDetokenizer())
+    return denoiser, generator
 
+
+def get_denoised(text, denoiser, generator, ctx):
+    """Denoise output."""
+    src_seq, src_valid_length = encode_char(text)
+    src_seq = mx.nd.array([src_seq], ctx=ctx)
+    src_valid_length = mx.nd.array(src_valid_length, ctx=ctx)
+    encoder_outputs, _ = denoiser.encode(src_seq, valid_length=src_valid_length)
+    states = denoiser.decoder.init_state_from_encoder(encoder_outputs,
+                                                      encoder_valid_length=src_valid_length)
+    inputs = mx.nd.full(shape=(1,), ctx=src_seq.context, dtype=np.float32, val=BOS)
+    output = generator.generate_sequences(inputs, states, text)
+    return output.strip()
+
+    
+@st.cache(suppress_st_warning=True)
+def denoise(decoded_lines_am, ctx):
+    denoiser, generator = load_denoiser(ctx)
+    
     progress_bar = st.progress(0)
     decoded_lines = []
     for j, decoded_line_am in enumerate(decoded_lines_am):
-        decoded_lines.append(get_denoised(decoded_line_am, denoiser, generator))
+        decoded_lines.append(get_denoised(decoded_line_am, denoiser, generator, ctx))
         progress_bar.progress(int((j + 1) / len(decoded_lines_am) * 100))
-    decoded_text = ' '.join(decoded_lines)
-    return decoded_text
+    return decoded_lines
 
 
 st.title("Demo: Handwriting Recognition for English")
@@ -128,18 +140,19 @@ if uploaded_file is not None:
     img = Image.open(uploaded_file)
     st.image(img, caption="Uploaded Image", use_column_width=True)
 
-    image = np.asarray(img.convert('L'))
+    image = np.asarray(img.convert("L"))
 
     st.write("\nSegmentation...")
-    line_images = segment(image)
+    line_images = segment(image, ctx)
 
     st.write("\nHandwriting recognition...")
-    decoded_lines_am = recognize(line_images)
+    decoded_lines_am = recognize(line_images, ctx)
 
     st.write("\nDenoising...")
-    decoded_text = denoise(decoded_lines_am)
+    decoded_lines = denoise(decoded_lines_am, ctx)
 
     st.write("Output:")
+    decoded_text = ' '.join(decoded_lines)
     st.write(decoded_text)
 
     reference = st.text_input("Input reference to compute CER and WER")
