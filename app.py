d5d76eb7c4926b5e877960fec06a47a71b2ad993
kesamet <kesamet@gmail.com>
2020-07-07 16:04:39 +0800

    Update

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import base64
+from pathlib import Path
+
 import streamlit as st
 
 from bert.app_ner import ner
@@ -6,7 +9,20 @@ from table_ocr.app_ocr import table_ocr
 from pose_estimation.app_pose import pose_compare
 # from handwriting_ocr_eng.app_ocr import handwriting_ocr
 
-    
+
+def uri_encode_path(path, mime="image/png"):
+    raw = Path(path).read_bytes()
+    b64 = base64.b64encode(raw).decode()
+    return f"data:{mime};base64,{b64}"
+
+
+def add_header(path):
+    st.markdown(
+        "<img src='{}' class='img-fluid'>".format(uri_encode_path(path)),
+        unsafe_allow_html=True,
+    )
+
+
 def main():
     # max_width = 900
     # st.markdown(
@@ -21,13 +37,18 @@ def main():
     # )
 
     select_page = st.sidebar.selectbox("Select demo", [
+        "",
         "Named Entity Recognition",
         "Image OCR",
         "Table OCR",
         "Pose Comparison",
     ])
 
-    if select_page == "Named Entity Recognition":
+    if select_page == "":
+        add_header("assets/logo.png")
+        st.title("Demos")
+        st.write("This app contains a series of demos. Select a demo in the left panel.")
+    elif select_page == "Named Entity Recognition":
         ner()
     elif select_page == "Handwriting Recognition for English":
         st.write("*Under Construction*")
--- pose_estimation/app_pose.py
+++ pose_estimation/app_pose.py
@@ -15,21 +15,23 @@ def get_score(filepath):
 def pose_compare():
     st.title("Pose Comparison Demo")
 
+    select_ex = st.selectbox("Select examples.", ["ex1", "ex2"])
+
     st.subheader("Video 1")
-    st.video(DATA_DIR + "crop1.mp4")
+    st.video(DATA_DIR + f"{select_ex}/crop1.mp4")
     
     st.subheader("Video 2")
-    st.video(DATA_DIR + "crop2.mp4")
+    st.video(DATA_DIR + f"{select_ex}/crop2.mp4")
     
     st.header("Pose Estimation of Target")
     st.write("A pose estimator model is used to detect the main body parts. "
              "The body parts form a connected graph.")
     
     st.subheader("Video 1: after pose estimation")
-    st.video(DATA_DIR + "out1.mp4")
+    st.video(DATA_DIR + f"{select_ex}/pose1.mp4")
     
     st.subheader("Video 2: after pose estimation")
-    st.video(DATA_DIR + "out2.mp4")
+    st.video(DATA_DIR + f"{select_ex}/pose2.mp4")
     
     st.header("Comparison Method")
     st.write("For each frame, we compare the (normalized) vectors formed by pairs of body parts "
@@ -52,7 +54,7 @@ def pose_compare():
     st.write("We then take the average of these similarity scores to get a mean score for each frame.")
     st.write("Finally, we take the average of these scores across frames.")
     
-    score = get_score(DATA_DIR + "scores.pkl")
+    score = get_score(DATA_DIR + f"{select_ex}/scores.pkl")
     st.write(f"**Mean similarity score = `{score:.4f}`**")
     
 
