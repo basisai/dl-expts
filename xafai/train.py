4b4e7db28f4b7c3373893b32dd6c4ecec2fa4e7b
kesamet <kesamet@gmail.com>
2020-07-08 08:43:07 +0800

    Update

--- xafai/train.py
+++ xafai/train.py
@@ -19,8 +19,8 @@ OUTPUT_MODEL_PATH = "/artefact/model.pkl"
 
 def compute_log_metrics(y_true, y_pred):
     """Compute and log metrics."""
-    acc = metrics.accuracy_score(y_true, y_pred)
-    f1_score = metrics.f1_score(y_true, y_pred)
+    acc = metrics.accuracy_score(y_true, y_pred, average="weighted")
+    f1_score = metrics.f1_score(y_true, y_pred, average="weighted")
     print("Evaluation\n"
           f"  Accuracy          = {acc:.4f}\n"
           f"  F1 score          = {f1_score:.6f}")
