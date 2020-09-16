66f4b680ab9d83263a5df1165e88337191024457
kesamet <kesamet@gmail.com>
2020-09-16 11:10:48 +0800

    Update detection demo

--- detection/app_detection.py
+++ detection/app_detection.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 DATA_DIR = "detection/samples/"
 
@@ -8,7 +9,7 @@ def detect():
 
     st.subheader("Methodologies")
     st.write("Objection detection algorithms:\n"
-             "- YOLOv3, [YOLOv5](https://github.com/ultralytics/yolov5)\n"
+             "- YOLOv4, [YOLOv5](https://github.com/ultralytics/yolov5)\n"
              "- [EfficientDet](https://github.com/rwightman/efficientdet-pytorch)")
     st.write("Animal pose estimation:\n"
              "- [DeepLabCut](https://github.com/DeepLabCut/DeepLabCut)\n"
@@ -19,7 +20,40 @@ def detect():
 
     st.subheader("Example: after applying detection algorithm (YOLO)")
     st.video(DATA_DIR + "out2.mp4")
-    
+
+    st.header("Pose estimation: prelims")
+    st.write("We will first need to label the different parts of the elephant. Preliminary list of body parts are ")
+    st.write("""
+        - L_eye
+        - R_eye
+        - L_ear
+        - R_ear
+        - Throat
+        - Tail
+        - Withers
+        - L_F_elbow
+        - R_F_elbow
+        - L_B_elbow
+        - R_B_elbow
+        - L_F_knee
+        - R_F_knee
+        - L_B_knee
+        - R_B_knee
+        - L_F_paw
+        - R_F_paw
+        - L_B_paw
+        - R_B_paw
+        - Mid_trunk
+        - End_trunk
+    """)
+    st.write("`Do they look ok?`")
+
+    st.subheader("Example of labelling an image frame")
+    st.image(DATA_DIR + "frame146_anno.jpeg")
+
+    st.subheader("Another example: horse")
+    st.image(DATA_DIR + "ho1_anno.jpeg")
+
 
 if __name__ == "__main__":
     detect()
